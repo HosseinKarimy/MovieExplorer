1d289104d9352b8ced5be1d9edfb27cd8290005e
hossein karimy <hosseinkm01@gmail.com>
2025-01-09 10:31:41 +0330

    Fix navbar for  show image and username

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -1,19 +1,23 @@
 <nav>
-    <div class="nav-right">
-        <a href="index.html">صفحه اصلی</a>
-        <a href="#">دسته بندی ها</a>
-        <a href="#">تماس با ما</a>
-        <a href="#" id="change-theme" onclick="changeColor()"> تغییر تم </a>
-    </div>
-     <div class="nav-left">
-        <?php
-          $islogin = false;
-          if($islogin)
-            echo '<span class="username">حسین</span>
-          <img src="assets\images\user_icon.png" alt="User Icon" class="user-icon">';
-          else 
-            echo '<a href="StaticPages\signup.html">ثبت‌نام</a>'
-        ?>  
-    </div> 
+  <div class="nav-right">
+    <a href="/Index.php">صفحه اصلی</a>
+    <a href="#">دسته بندی ها</a>
+    <a href="#">تماس با ما</a>
+    <a href="#" id="change-theme" onclick="changeColor()"> تغییر تم </a>
+  </div>
+  <div class="nav-left">
+    <?php
+    $islogin = isset($_SESSION['user_id']);
+    if ($islogin) {
+      // If the user is logged in, show the username and user icon
+      echo '<span class="username">' . $_SESSION['username'] . '</span> 
+    <img src="/PHP/' . $_SESSION['profile_image'] .  '" alt="User Icon" class="user-icon">';
+    } else {
+      // If the user is not logged in, show the signup link
+      echo '<a href="StaticPages/signup.html">ثبت‌نام</a>';
+    }
+    ?>
+
+  </div>
 
 </nav>
