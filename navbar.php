ec039418f6cc09c0b9437120c8f37cb78855ff5d
hossein karimy <hosseinkm01@gmail.com>
2025-01-22 00:29:20 +0330

    Logout feature

--- navbar.php
+++ navbar.php
@@ -3,21 +3,19 @@
     <a href="/Index.php">صفحه اصلی</a>
     <a href="#">دسته بندی ها</a>
     <a href="#">تماس با ما</a>
-    <a href="#" id="change-theme" onclick="changeColor()"> تغییر تم </a>
+    <a href="#" id="change-theme" onclick="changeColor()">تغییر تم</a>
   </div>
   <div class="nav-left">
     <?php
     $islogin = isset($_SESSION['user_id']);
     if ($islogin) {
-      // If the user is logged in, show the username and user icon
-      echo '<a href="/user.php" class="username">' . $_SESSION['username'] . '</a> 
-    <img src="/' . $_SESSION['profile_image'] .  '" alt="User Icon" class="user-icon">';
+      echo '<a href="/logout.php" class="logout"> خروج </a>';
+      echo '<a href="/user.php" class="username">' . htmlspecialchars($_SESSION['username']) . '</a>';
+      echo '<img src="/' . htmlspecialchars($_SESSION['profile_image']) . '" alt="User Icon" class="user-icon">';
     } else {
       // If the user is not logged in, show the signup link
       echo '<a href="/StaticPages/signup.html">ثبت‌نام</a>';
     }
     ?>
-
   </div>
-
 </nav>
